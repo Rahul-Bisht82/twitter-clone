e83d26c451621d501b681becfcc815f67d07a230
rahul bisht <rahul.b4749@gmail.com>
2020-08-13 12:12:01 +0530

    Updates

---
+++ precache-manifest.7e72338b44465e4ea7175d9209449ba7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db9fc0b256c00c9b6a14d02ef5dd9185",
+    "url": "/twitter-clone/index.html"
+  },
+  {
+    "revision": "046621e277ce78977d7b",
+    "url": "/twitter-clone/static/css/main.b2353a36.chunk.css"
+  },
+  {
+    "revision": "d519ad7cd9877fea9c0e",
+    "url": "/twitter-clone/static/js/2.58aadea1.chunk.js"
+  },
+  {
+    "revision": "a8de4128b679d6159aad8871b5569c6d",
+    "url": "/twitter-clone/static/js/2.58aadea1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "046621e277ce78977d7b",
+    "url": "/twitter-clone/static/js/main.021a33ac.chunk.js"
+  },
+  {
+    "revision": "4ea1c8ca6c4e7890228d",
+    "url": "/twitter-clone/static/js/runtime-main.a74d6e56.js"
+  }
+]);
